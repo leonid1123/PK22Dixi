099b9f226b7aa789e7d49e5aa0785731e266e948
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-10-31 11:48:32 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Tovar yabloki1 = new Tovar();
+
+string fileName = "yabloki.txt";
+using (StreamReader sr = new StreamReader(fileName))
+{
+    string line;
+    while ((line = sr.ReadLine()) != null)
+    {
+        Console.WriteLine(line);
+    }
+}
+class Tovar 
+{
+    string nazvanie;
+    string edIzm;
+    int czena;
+    float kolichestvo;
+    string tip;
+    float skidka;
+    int srokGodnosti;
+    public void PrintInfo(){
+        Console.WriteLine("Название:{0}, {1}руб. за кг, в наличии:{2}{3},тип товара:{4}, величина скидки:{5}, срок годности:{6} дней",nazvanie,czena,kolichestvo,edIzm,tip,skidka,srokGodnosti);
+    }
+    public Tovar(string _nazvanie,string _edIzm, int _czena, float _kolichestvo, string _tip, float _skidka, int _srokGodnosti){
+        nazvanie = _nazvanie;
+        edIzm = _edIzm;
+        czena = _czena;
+        kolichestvo = _kolichestvo;
+        tip = _tip;
+        skidka = _skidka;
+        srokGodnosti = _srokGodnosti;
+
+    }
+}
